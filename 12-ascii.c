9e23cea4893a1a71b8caac8738b0c8262abbf8d8
pulkit agarwal <pulkitagarwal877@gmail.com>
2020-09-13 13:04:57 +0530

    finding ascii value

---
+++ 12-ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    char ascii;
+    printf("Enter a character:");
+    scanf("%c",&ascii);
+    printf("The ascii value of %c is %d",ascii,ascii);
+    return 0;
+}
