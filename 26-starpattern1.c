ef188dcee00aef19acbdf009fea8ca7e2829ecba
pulkit agarwal <pulkitagarwal877@gmail.com>
2020-10-02 12:42:49 +0530

    pattern

---
+++ 26-starpattern1.c
@@ -0,0 +1,21 @@
+//triangle star pattern:
+/*
+*
+**
+***
+**** 
+*/
+#include<stdio.h>
+void main()
+{ 
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("*");
+    }
+        printf("\n");
+    }
+    return 0;
+}
